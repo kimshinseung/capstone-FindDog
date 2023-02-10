65ae22fb0e2960d65899ad38f92b129daf2ebea6
kimshinseung <94797657+kimshinseung@users.noreply.github.com>
2023-02-10 16:42:41 +0900

    Revert "map.js"
    This reverts commit 7a65a1d02ba7f37095cc1faa4fb7c2461fcd69d2.

--- src/components/page/Pages.js
+++ src/components/page/Pages.js
@@ -10,7 +10,6 @@ import { useLocation, Outlet } from "react-router-dom";
 import { MainMenu, MissMenu, FindMenu, HospitalMenu, SearchMenu } from "../menu/Menu";
 import LoginPage  from "./Login/LoginPage";
 
-import Map from "../map";
 
 // Page Template
 const PageTemplate = ({ children }) => (
@@ -86,9 +85,9 @@ export const Hospital = () => (
   </PageTemplate>
 );
 
-export const HospitalSub1 = () => (  
+export const HospitalSub1 = () => (
   <div>
-    <Map/>
+    <h3>보호소 및 동물병원/SUB1</h3>
   </div>
 );
 
