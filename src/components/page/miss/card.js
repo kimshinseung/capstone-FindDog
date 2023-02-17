f762e2405c0ac19b85c922c8e50409e73761c8e0
ysujin <91712467+ysujin@users.noreply.github.com>
2023-02-17 17:39:43 +0900

    Card 이미지 출력

--- src/components/page/miss/card.js
+++ src/components/page/miss/card.js
@@ -5,7 +5,7 @@ function Card(props) {
     return (
       <>
         <Col>
-          <img src={`/${props.i}.jpg`} />
+          <img src={`images/missfind${props.i}.jpg`} width={300} height={300} />
           <h4>{props.profiles.title}</h4>
           <p>{props.profiles.content}</p>
           <p>{props.profiles.price}</p>
