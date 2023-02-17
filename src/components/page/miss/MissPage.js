37970b03b60040d4d3591401b2e6582d3edfc76c
godi00 <112918876+godi00@users.noreply.github.com>
2023-02-17 17:14:11 +0900

    Update MissPage.js

--- src/components/page/miss/MissPage.js
+++ src/components/page/miss/MissPage.js
@@ -10,10 +10,10 @@ import { useNavigate, Link } from "react-router-dom";
 
 
 export const MissPage = () => {
-    //let [profiles] = useState(data);
+    let [profiles] = useState(data);
 
     return (
- <>
+        <>
             <div className="miss-page">
                 <h2>실종 게시판</h2>
                 <Link to="uplode"> 실종 등록하기</Link>
@@ -21,7 +21,14 @@ export const MissPage = () => {
                 <h3>최근 실종 순</h3> 
                 <Link to="moreInfo"> 더보기</Link>
                 <br/>
+
+                <Container>
+                <Row>
+                {profiles.map((item, i) => <Card profiles={item} i={i+1} key={item.id} />)}
+                </Row>
+                </Container>
             </div>
+        
         </>
     );
 };
