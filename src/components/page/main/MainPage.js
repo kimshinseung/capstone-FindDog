3c7da92f35eb473ba53d360df24032da0a421458
Yunseohee <mango08076@naver.com>
2023-02-27 16:34:27 +0900

    Revert "Mainpage banner, youtubeplayer 삽입"
    This reverts commit a85088385293d24c538eb6f445debb98ba57bcd8.

--- src/components/page/main/MainPage.js
+++ src/components/page/main/MainPage.js
@@ -2,24 +2,12 @@
  * ./src/components/page/main/MainPage.js
  * 메인 홈
  */
-import BannerCarousel  from "./banner";
-import React from "react";
-import ReactPlayer from 'react-player'
 
 export const MainPage = () => {
     return (
         <>
             <div className="main-page">
-                <BannerCarousel/>
-                <br/><br/>
-                <ReactPlayer className="video" 
-                url="https://www.youtube.com/watch?v=XMsDXtFWJ2U" 
-                width="520px"
-                height="320px"
-                playing={true}
-                muted={true}
-                controls={true}
-                />
+                <h3>메인 페이지...</h3>
             </div>
         </>
     );
