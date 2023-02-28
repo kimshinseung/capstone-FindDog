e0bbf870ae9f442e8afff1583cb94a45916818b4
ysujin <91712467+ysujin@users.noreply.github.com>
2023-02-28 14:24:26 +0900

    Upload 코드정리, 주소입력

--- src/components/page/miss/UploadPage.js
+++ src/components/page/miss/UploadPage.js
@@ -3,9 +3,10 @@
  * 실종 등록하기
  */
 import "./UploadPage.scss";
-//import userInputs from "./formData.js";
+import userInputs from "./formData.js";
 import { React, useState, useEffect} from "react";
 import { useNavigate, Link } from "react-router-dom";
+import DaumPostcode from "react-daum-postcode";
 import { addDoc, collection } from "@firebase/firestore";
 import { db, storage } from "../../../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
@@ -13,13 +14,12 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 
 const UploadPage = () => {
-    const inputs = ["품종", "성별", "털색"];
-    const specifyList = ["말티즈", "시바", "허스키"];
-    const genderList = ["수컷", "암컷", "중성화"];
-    const farColorList = ["검은색", "갈색", "하얀색"];
+    //const inputs = ["품종", "성별", "털색"];
 
     const [data, setData] = useState({});
     const [file, setFile] = useState("");
+    const [address, setAddress] = useState("");
+    const [popup, setPopup] = useState(false);
 
     useEffect(()=>{
         const uploadFile= () => {
@@ -63,17 +63,75 @@ const UploadPage = () => {
         const id = e.target.id;
         const value = e.target.value;
         setData({ ...data, [id]: value });
+
     };
 
 
     const handler = async(e) =>{
         e.preventDefault();
-        const res = await addDoc(collection(db, "Missing" ), {
+        await addDoc(collection(db, "Missing" ), {
             ...data
             //time: serverTimestamp()
         });
-        console.log(res.id);
+        alert("등록되었습니다");
     }
+
+
+
+    // const handlePostCode = (data) => {
+    //     let fullAddress = data.address;
+    //     let extraAddress = ''; 
+        
+    //     if (data.addressType === 'R') {
+    //         if (data.bname !== '') {
+    //             extraAddress += data.bname;
+    //         }
+    //         if (data.buildingName !== '') {
+    //             extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+    //         }
+    //         fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+    //     }
+    //     console.log(data)
+    //     console.log(fullAddress)
+    //     console.log(data.zonecode)
+    //     props.onClose()
+
+        
+    // }
+
+    // const onCompletePost = data => {
+    //     setModalState(false);
+    //     setAddress(data.address);
+    // };
+
+    const handleComplete = (data) => {
+        let fullAddress = data.address;
+        let extraAddress = '';
+        if (data.addressType === 'R') {
+            if (data.bname !== '') {
+                extraAddress += data.bname;
+            }
+            if (data.buildingName !== '') {
+                extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+            }
+            fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+        //fullAddress -> 전체 주소반환
+        //setPopup(false);
+        setAddress(data.address);
+    }
+
+    const postCodeStyle = {
+        display: "block",
+        position: "absolute",
+        top: "10%",
+        width: "500px",
+        height: "600px",
+        padding: "30px",
+        
+    };
+
+
 
 
     return (
@@ -81,50 +139,24 @@ const UploadPage = () => {
             <div className="upload-page">
                 <form onSubmit = {handler}>
 
-                    {/* {userInputs.map((input)=>(
+                    {userInputs.map((input)=>(
                         <div>
                             <h3>{input.label}</h3>
                             <select id={input.id} onChange={handleInput}>
                                 <option disabled selected>-------</option>
-                                {input.datas}.map((item)=>(
+                                {input.datas.map((item)=>(
                                     <option value={item} key={item}>{item}</option>
-                                ))
+                                ))}
                             </select>
                         </div>
-                    ))} */}
+                    ))} 
 
-                    <div>
-                    <h3>품종</h3>
-                    <select id="Specify" onChange={handleInput}>
-                        <option disabled selected>-------</option>
-                        {specifyList.map((item)=>(
-                            <option value={item} key={item}>{item}</option>
-                        ))}
-                    </select>
-                    </div>
                     
-                    <div>
-                    <h3>성별</h3>
-                    <select id="Male" onChange={handleInput}>
-                    <option disabled selected>-------</option>
-                        {genderList.map((item)=>(
-                            <option value={item} key={item}>{item}</option>
-                        ))}
-                    </select>
-                    </div>
-
-                    <div>
-                    <h3>털색</h3>
-                    <select id="FarColor" onChange={handleInput}>
-                        <option disabled selected>-------</option>
-                        {farColorList.map((item)=>(
-                            <option value={item} key={item}>{item}</option>
-                        ))}
-                    </select>
-                    </div>
 
                     <h3>실종장소</h3>
-                    <input type="text" id="place" onChange={handleInput}></input>
+                    {popup && <div><DaumPostcode style={postCodeStyle} onComplete={handleComplete}/></div>}
+                    <input type="text" id="place" onChange={handleInput} value={address}></input>
+                    <input type="button" onClick={()=>setPopup(true)} value="주소찾기"></input>
 
                     <h3>성격 및 특징</h3>
                     <textarea cols="30" rows="5" id="place" onChange={handleInput}></textarea>
--- src/components/page/miss/formData.js
+++ src/components/page/miss/formData.js
@@ -1,17 +1,19 @@
-export default userInputs = [
+const userInputs = [
     {
         id: "Specify",
         label: "품종",
-        datas: specifyList
+        datas: ["말티즈", "시바", "허스키"]
     },
     {
         id: "Gender",
         label: "성별",
-        datas: genderList
+        datas: ["수컷", "암컷", "중성화"]
     },
     {
         id: "FarColor",
         label: "털색",
-        datas: farColorList
+        datas: ["검은색", "갈색", "하얀색"]
     }
 ];
+
+export default userInputs;
