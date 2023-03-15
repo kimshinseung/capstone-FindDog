1d930af74600d71cd0c26f1a4e044ef469be08b8
kimshinseung <94797657+kimshinseung@users.noreply.github.com>
2023-03-15 18:27:31 +0900

    검색수정1

---
+++ src/components/page/search/Post.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Post=({postObj})=>{
+  
+  //검색하기 전 null일 때 상태 값
+  if(postObj[0]==null){
+    return(
+      <div>
+        <h4>데이터가 없습니다</h4>
+      </div>
+    )
+  }
+  //검색 후 실행되는 부분
+  else{
+    //console.log(postObj);
+    return(
+      <div>
+        <h4>{postObj[0].name}</h4>
+      </div>
+    );
+  }
+//  console.log(postObj[0])
+//   return(
+//     <div>
+//       <h4>{postObj.id}</h4>
+//     </div>
+//   );
+};
+
+
+export default Post;
--- src/components/page/search/SearchPage.js
+++ src/components/page/search/SearchPage.js
@@ -8,68 +8,48 @@ import React, { useEffect } from "react";
 import { db, storage } from "../../../firebase";
 import { getDoc, getDocs, collection,updateDoc, addDoc, query, where, orderBy, QuerySnapshot, doc } from "firebase/firestore";
 import { useState } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Card from './card';
-//import data from './data';
+import Post from "./Post";
+import { post } from "jquery";
 
 
 
 export function SearchPage() {
 
-  let data = [
-    {
-      id : 0,
-      name : "",
-      missedplace : ""
-    },
-  
-    {
-      id : 1,
-      name : "",
-      missedplace : ""
-    },
-  
-    {
-      id : 2,
-      name : "라떼",
-      missedplace : "남위례역 앞"
-    },
-  ]
-    let [profiles] = useState(data);
-
     useEffect(() => {
        Search();
     }, []);
    
+    const [postdata, setpost] = useState(new Map());
+
+
     const find = async (specify, gender, farColor) => {
         const q = query(collection(db, "Missing"), where("specify", "==", specify),
             where("gender", "==", gender), where("farColor", "==", farColor)
             );
-        
         const QuerySnapshot = await getDocs(q);
-        let count=0;
-        QuerySnapshot.forEach((doc) => {
-            
-            let docs= doc.data();// docs에 doc데이터들 다 받아옴
-            //let str=document.getElementById("Info");
-            //str.innerText+= "\n"+url;
-            profiles[count].name=docs["name"];
-            console.log(profiles[count].name);
-            profiles[count].missedplace=docs["address"];
-            console.log(profiles[count].missedplace);
-            count++;
-
-        })
-        return alert("asd")
+        
+       
+           const data = QuerySnapshot.docs.map((doc)=>({
+            id: doc.id,
+            ...doc.data(),
+           }));
+        setpost(data);
+       
     }
     
     //아직 이 함수 못 씀
     //게시물들 찾아주는 함수 만들어야됨
-    const Show = async(id) => { //id 받아와서 게시물 생성
-        let str=document.getElementById("Info");
-        str.innerText=id;
+    const Show = async(q) => { //id 받아와서 게시물 생성
+      const QuerySnapshot = await getDocs(q);
+      let count=0;
+      QuerySnapshot.forEach((doc) => {
+          
+          let docs= doc.data();// docs에 doc데이터들 다 받아옴
+          //profiles[count].name=docs["name"];
+          count++;
+          console.log(docs);
+
+      })
     }
 
     //파이어베이스 스토리지에서 이미지 가져오기
@@ -78,42 +58,7 @@ export function SearchPage() {
        
     }
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
-                  dots: true
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
-            ]
-        }
-
-    const sa =()=>{
-      return alert();
-    }
+  
     const Search = () => {
         let element = document.querySelector("#specify");
         let specify = element.value;
@@ -158,16 +103,13 @@ export function SearchPage() {
                 </select>
             </div>
             <button
-                onClick={() => {Search();}}>
+                onClick={() => {Search()}}>
                 검색</button>
                 <div>
             </div>
-
-            <div className="carousel">
-            <Slider {...settings}>
-              {profiles.map((item, i) => <Card profiles={item} i={i+1} key={item.id} />)}
-            </Slider>
-        </div>
+            <div>
+                    <Post postObj={postdata}/> 
+            </div>
         </div>
     </>
 
