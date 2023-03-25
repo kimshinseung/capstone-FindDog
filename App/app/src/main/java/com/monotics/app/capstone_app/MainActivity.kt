0305758b798df7d8c326fa42db561bb69f4cc1ab
kimshinseung <94797657+kimshinseung@users.noreply.github.com>
2023-03-25 18:50:09 +0900

    map 하는중

--- App/app/src/main/java/com/monotics/app/capstone_app/MainActivity.kt
+++ App/app/src/main/java/com/monotics/app/capstone_app/MainActivity.kt
@@ -2,12 +2,18 @@ package com.monotics.app.capstone_app
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.monotics.app.capstone_app.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -15,13 +21,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+
         binding.profile.setOnClickListener{
             val intent = Intent(this,ProfileActivity::class.java)
             startActivity(intent)
         }
-
-
         //데이터베이스 불러옴
         val db = Firebase.firestore
     }
+
 }
--- App/app/src/main/java/com/monotics/app/capstone_app/ProfileActivity.kt
+++ App/app/src/main/java/com/monotics/app/capstone_app/ProfileActivity.kt
@@ -1,11 +1,14 @@
 package com.monotics.app.capstone_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.monotics.app.capstone_app.databinding.ActivityMainBinding
 import com.monotics.app.capstone_app.databinding.ActivityProfileBinding
+import kotlinx.android.synthetic.main.activity_profile.*
+import net.daum.mf.map.api.MapView
 
 class ProfileActivity : AppCompatActivity()  {
     val binding by lazy { ActivityProfileBinding.inflate(layoutInflater) }
@@ -13,5 +16,15 @@ class ProfileActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val mapView = MapView(this)
+        val mapViewContainer = map_View
+        mapViewContainer.addView(mapView)
+
+        binding.HomeButton.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
