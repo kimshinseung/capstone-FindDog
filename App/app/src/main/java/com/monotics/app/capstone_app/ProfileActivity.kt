714fb661940f273c862f7f757ba42a431d5e83c2
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-03-30 19:16:00 +0900

    로그인 성공

--- App/app/src/main/java/com/monotics/app/capstone_app/ProfileActivity.kt
+++ App/app/src/main/java/com/monotics/app/capstone_app/ProfileActivity.kt
@@ -2,7 +2,10 @@ package com.monotics.app.capstone_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.monotics.app.capstone_app.databinding.ActivityMainBinding
@@ -17,15 +20,41 @@ class ProfileActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //로그인되어있을때 실행하는 함수
+        if(Firebase.auth.currentUser != null){
+            //startActivity(Intent(this,SearchActivity::class.java))
+        }
+
+        //로그인버튼
+        binding.loginButton.setOnClickListener {
+            doLogin(binding.editEmail.text.toString(),binding.editPassword.text.toString())
+        }
 
         //홈으로 돌아가기
         binding.HomeButton.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+        //회원가입 화면
         binding.createAccount.setOnClickListener {
             startActivity(Intent(this,SigninActivity::class.java))
         }
+    }
+
+    fun doLogin(id:String, pw: String){
+        Firebase.auth.signInWithEmailAndPassword(id,pw)
+            .addOnCompleteListener(this) {
+                println("Login Task Occur")
+
+                if(it.isSuccessful){
+                    Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                else{
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 }
