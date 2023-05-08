6a3d7329fd9e5fbbe46fa255357759d5e36d3f4e
godi00 <112918876+godi00@users.noreply.github.com>
2023-05-09 08:38:02 +0900

    자유게시판
    수정해야 할 것
    - 글 올릴 때 새로고침 해야 됨
    - 게시글 필드 정립>시간순 정렬 맞추기

--- src/components/page/forum/ForumPage.js
+++ src/components/page/forum/ForumPage.js
@@ -3,11 +3,36 @@
  * 자유 게시판
  */
 
-import React from "react";
-import { Pagination } from "./Pagination";
+import React, { useState, useEffect } from "react";
+import Pagination from "./Pagination";
 import { useNavigate } from "react-router-dom";
 
+import { getDocs, collection, query } from "firebase/firestore";
+import { db } from "../../../firebase";
+
 export const ForumPage = () => {
+    const [posting, setPosting] = useState([]);
+    
+    const [limit, setLimit] = useState(10); // 페이지 당 게시물 수(기본 10개씩)
+    const [now, setNow] = useState(1);      // 현재 페이지 번호(시작은 첫번째)
+    const offset = (now - 1) * limit;       // 각 페이지의 첫 게시물 위치
+
+    useEffect(async () => {
+        // firebase의 Forum Collection에서 글 목록 가져오기
+        setPost();
+        
+    }, []);
+
+    const setPost = async () => {
+        const QuerySnapshot = await getDocs(query(collection(db, "Forum")));
+        const data = QuerySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        // console.log(Array.from(data));
+        setPosting(Array.from(data));
+        // console.log(posting);
+    }
 
     const navigate = useNavigate();
 
@@ -19,10 +44,40 @@ export const ForumPage = () => {
         <>
         <div className="forum-page">
             <h2>자유게시판</h2>
+            
+            한 페이지 당 표시할 게시물 수: 
+            {/* 페이지 당 게시물 수: {limit} */}
+            <select type="number" value={limit}
+            onChange={({ target: { value } }) => setLimit(Number(value))}
+            >
+                <option value="3">3</option>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+            </select>
+            <br/>
+            {/* 현재 페이지 번호: {now}<br/>
+            각 페이지의 첫 게시물 위치: {offset}<br/><br/> */}
+
             <br/>
             <button onClick={toUploadHandler}>글 올리기</button>
             <br/><br/>
-            <Pagination/>
+            <div className="forum-posts">
+            {posting.slice(offset, offset + limit).map(({ text, id }) => (
+                
+                <p className="forum-posting" key={id}>{text}</p>
+
+            ))}
+            </div>
+            <footer>
+            <Pagination
+            total={posting.length}
+            limit={limit}
+            now={now}
+            setNow={setNow}
+            />
+            </footer>
         </div>
         </>
     );
