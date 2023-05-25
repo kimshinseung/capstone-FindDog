9045861856e9098a0266b64d31cac5d88944ed44
Yunseohee <mango08076@naver.com>
2023-05-25 20:34:35 +0900

    MissPage css

--- src/components/page/miss/MissPage.js
+++ src/components/page/miss/MissPage.js
@@ -5,25 +5,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Carousel  from "./carousel";
+import "./MissPage.scss";
 
 
 export const MissPage = () => {
     return (
         <>
             <div className="miss-page">
+                <div className="miss-page-upload-btn">
                 <h2>실종 게시판</h2>
                 <Link to="upload"> 실종 등록하기</Link>
+                <button type="button" onclick="location.href='UploadPage.js'">실종 등록하기</button>
+                </div>
 
-                <br/><br/><br/>
-                <h3>최근 실종 순</h3> 
-                <Link to="moreInfo"> 더보기</Link>
                 <br/>
+                <div className="misspage-moreInfo-btn">
+                <h3>최근 실종 순</h3> 
+                <Link to="moreInfo">더보기</Link>
+                </div>
                 <Carousel category={"Missing"} cg={"moreInfo"}/>
 
-                <br/><br/><br/>
+                <br/><br/>
+                <div className="miss-page-moreInfo2-btn">
                 <h3>우리 지역 관련 순</h3>
-                <Link to="moreInfo2"> 더보기</Link> 
-                <br/>
+                <Link to="moreInfo2">더보기</Link> 
+                </div>
+
                 <Carousel category={"Missing"} cg={"moreInfo2"}/>
             </div>
         </>
