3c5f4f6e62c7bbe0fd58bb048eeea96db7eaeaca
godi00 <112918876+godi00@users.noreply.github.com>
2023-05-27 19:00:27 +0900

    헤더 로고 엑박 문제
    사실 해결된 건지는 모름

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,14 +32,14 @@ const Header = () => {
     
                 <div className='header-logo-div'>
                     <Link to="/">
-                        <img className='header-logo' src='images/logo_web.png' />
+                        <img className='header-logo' src={process.env.PUBLIC_URL + 'images/logo_web.png'} />
                     </Link>
                 </div>
     
                 <div className='header-profile-div'>{
                         (userState)
-                        ? (<Link to="/mypage"><img className='header-profile' src='images/user.png' /></Link>)
-                        : (<Link to="/login"><img className='header-profile' src='images/user.png' /></Link>)
+                        ? (<Link to="/mypage"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
+                        : (<Link to="/login"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
                     }
                 </div>
             </div>
