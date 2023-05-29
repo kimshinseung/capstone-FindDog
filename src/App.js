1198218f2c51673340ea753fd0c7f755a5ea589d
Yunseohee <mango08076@naver.com>
2023-05-29 16:44:35 +0900

    Up button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@
 
 // import Layout
 import Layout from './components/Layout';
+import UpButton from './components/topBtn';
 
 // App
 const App = () => (
+  <>
+  <div id="up" />
   <Layout />
+  <UpButton/>
+  </>
 );
 
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,11 +6,13 @@
 import Header from "./Header";
 import Hierarchy from "./menu/Hierarchy";
 import Footer from "./footer.js";
+import UpButton from './topBtn.js';
 
 // 레이아웃 정의
 const Layout = () => {
     return(
         <div>
+            <div id="up" />
             <Header />
             <Hierarchy />
             <Footer/>
