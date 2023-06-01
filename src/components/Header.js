58980c7da821f4a333c753c15f2dd2fe4f020601
Yunseohee <mango08076@naver.com>
2023-06-01 16:46:07 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
     
                 <div className='header-logo-div'>
                     <Link to="/">
-                        <img className='header-logo' width="100%" height="100%" src={process.env.PUBLIC_URL + 'images/logo_web.png'} />
+                        <img className='header-logo'  src={process.env.PUBLIC_URL + 'images/logo_web.png'} />
                     </Link>
                 </div>
     
